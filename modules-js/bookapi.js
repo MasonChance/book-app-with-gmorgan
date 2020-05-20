79d9a80ddc99f529ddd27b04795c20e31f3f2b97
MasonChance <mason.fryberger@gmail.com>
2020-05-19 20:05:12 -0700

    now getting correct info from form to the route

--- modules-js/bookapi.js
+++ modules-js/bookapi.js
@@ -23,15 +23,16 @@ function Book(obj){
   this.synopsis = obj.volumeInfo.description || 'Description unavailable at this time.';
 };
 
-Book.Query = function (q, terms){ // whatever we pass as terms will be determined by the post from the form. 
+Book.Query = function (req){ // whatever we pass as terms will be determined by the post from the form. 
   // constructs a query object for superagent to use query= '?q=search+terms'
   this.key = process.env.GOOGLE_API_KEY;
-  this.url = 'https://www.googleapis.com/books/v1/volumes';
-  this.terms = terms ? inauthor : intitle;  /*intitle || inauthor; passed as result of form submit.*/
+  
+  // this.terms = terms ? inauthor : intitle;  /*intitle || inauthor; passed as result of form submit.*/
   this.q = req.query;/*'Stephen King';*/
 };
 
 function displayResults(req, res){ 
+  console.log(`req.query.search@ displayResults:bookapi  ${req.body.search}`);
   res.send(superQuery(req, res));
 }
 
@@ -39,13 +40,18 @@ function displayResults(req, res){
 //display results. 
 function superQuery(req, res){
   // console.log(`request.body[0].item: ${req.body[0].item}`);
-  console.log(`req.query ${req.query}`);
-  const bookListQuery = new Book.Query(req.query, req.query.terms)
-  superagent.get(bookListQuery.url)
-    .query(bookListQuery)
-    .then(result => makeItSo(result))
+  // console.log(`req.query.key: ${req.query.key}`);
+  // const bookListQuery = new Book.Query(req.query, req.query.terms)
+  const url = 'https://www.googleapis.com/books/v1/volumes';
+  superagent.get(url)
+    .query({'q': req.body.search, 'key' : process.env.GOOGLE_API_KEY})
+    .then(result => {
+      console.log(`result:fn:superagent:Object.keys: result.body ${Object.entries(result.body.items[0])}`)
+      makeItSo(result)
+    })
     .catch(error => {
-      res.redirect(error, '..veiws/pages/error.ejs')
+      console.log(`error from .catch: ${error}`)
+      // res.redirect(error, '..veiws/pages/error.ejs')
     })
 };
 
