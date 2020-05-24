20a1261f9d87bf0801bb107dbd67f2a338f5dc41
MasonChance <mason.fryberger@gmail.com>
2020-05-23 19:01:03 -0700

    allswell in the details

--- server.js
+++ server.js
@@ -39,63 +39,31 @@ app.get('/pages/index.ejs', (req, res) => {
 
 app.get('/pages/searches/new', (req, res) => res.render('pages/searches/new'));
 
-// app.get('/pages/searches/show,', )
+
 app.post('/pages/searches/show', displayResults);
 
-app.get('/book/:id', (req, res) => {
-  //Display the single book
-  // callback should pull data from DB and
-  console.log('req.body', req.params);
-  res.render('pages/detail',console.log('hello from app.post(\'/book\''))
-})
-
-app.post('/book', (req, res) => { 
-  sqlSave(req) // saves to sql
-  
-  res.redirect(`/book/${getArchivedId(req.body.title)}`); // try also book/:id=${getArchivedId(req.body.title)}.
-  // !!! will this actually pass the id to showSavedBooks or would I need to chain all of these into a .then sequence within a broadscope call back function for the app.post(/book) route???
-  
-  //FIXME: !!! how do we pass this Id to our details page so that the details callback can use it to fetch the data from the database and render it to the details page. !!! 
-});
+app.get('/book/:id', showSavedBook)
+app.post('/book', sqlSave);
 
 
-//!!! this 'detail' route may be redundant. it seems that the /book/:id route IS the 'detail' veiw route for the detail.ejs not a '/detail' route. 
-app.get('/pages/detail', (req, res) => {
-  client.query('SELECT * FROM books WHERE id $1', [req.params.id]).then(dataFromSql => {
-    
-  res.render('/pages/detail', dataFromSql.rows[0])
-  })
-});
+app.get('/pages/error', (req, res) => res.render('/pages/error'));
 
-app.get('/pages/searches/error', (req, res) => res.render('/pages/searches/error'));
-
-// Saves book to Database.called By 
+// Saves book to Database.called in app.post(/book) 
 function sqlSave(req, res)  {
-  const sqlSaveToDatabase = 'INSERT INTO books (author, title, image_url, description) VALUES ($1,$2,$3,$4)';
+  const sqlSaveToDatabase = 'INSERT INTO books (author, title, image_url, description) VALUES ($1,$2,$3,$4) RETURNING id';
   //FIXME:  add isbn to constructor, add to sqlSaveArr below, add to sqlSaveToDatabase above in parenthesis, add $5 to sqlSaveToDataBase above. 
   const sqlSaveArr = [req.body.author, req.body.title, req.body.img, req.body.synopsis]  
   client.query(sqlSaveToDatabase, sqlSaveArr)
+  .then(id => res.redirect('book/' + id.rows[0].id))
+  .catch(error => errorHandler(req, res, error))
 }
 
-
-
-function getArchivedId(req, res){
-  //needs, id from database.
-  const sqlSearch = 'SELECT * FROM books WHERE title=$1';
-  const sql =[req.body.title];
-  client.query(sqlSearch, sql)
-  // access ID result.rows[0].id
-    .then(record => (req, res)=> res.send(record.rows[0].id))
-    .catch(error => errorHandler(req, res, error))
-  // returns ID from DataBase
-}
-
-//sends object literals to the appropriate route for rendering and acces via ejs. 
-function showSavedBook(req, res, id){
+//sends object literals to the appropriate route for rendering and acces via ejs. !!! must be updated for IBN inclusion. 
+function showSavedBook(req, res){
   const sqlSelect = 'SELECT * FROM books WHERE id=$1';
-  const sqlValue = [id];
+  const sqlValue = [req.params.id];
   client.query(sqlSelect, sqlValue)
-    .then(sqlres => res.send({'id' : id}, {'title' : sqlres.rows[0].title}, {'author' : sqlres.rows[0].author}, {'synopsis' : sqlres.rows[0].synopsis}))
+    .then(sqlres => res.render('pages/detail', {'sqlres' : sqlres.rows[0], 'displaybutton' : true}))
     .catch(error => errorHandler(req, res, error))
 }
 
